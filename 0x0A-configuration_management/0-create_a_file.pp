3e15123556b1228f2be9932ef07b70d7dec6cc2e
Ade3164 <adebayoidris051@gmail.com>
2022-09-16 13:59:35 +0100

    file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# file in tmp school
+file { 'school':
+ensure  => file,
+  path  => '/tmp/school',
+  mode  => '0744',
+  owner => 'www-data',
+  group => 'www-data',
+content => 'I love Puppet'
+}
