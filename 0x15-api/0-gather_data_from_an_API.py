5e30fd03648006bb5e72dc18c8880c1a039bc34f
Ade3164 <adebayoidris051@gmail.com>
2022-10-19 23:11:13 +0100

    python

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,22 +1,30 @@
-  GNU nano 6.2                                                          0-gather_data_from_an_API.py                                                                    #!/usr/bin/python3
-"""
-Uses https://jsonplaceholder.typicode.com REST API for a given employee ID
-to return information about his/her TODO list progress
-"""
+#!/usr/bin/python3
+"""For a given employee ID, returns information about
+their TODO list progress"""
+
 import requests
-from sys import argv
+import sys
 
 if __name__ == "__main__":
-    userID = argv[1]
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
-                        format(userID)).json()
-    todos = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
-                         .format(userID)).json()
-    completed_tasks = []
-    for task in todos:
-        if task.get('completed') is True:
-            completed_tasks.append(task.get('title'))
-    print("Employee {} is done with tasks({}/{}):".
-          format(user.get('name'), len(completed_tasks), len(todos)))
-    print("\n".join("\t {}".format(task) for task in completed_tasks))
 
+    userId = sys.argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(userId))
+
+    name = user.json().get('name')
+
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    totalTasks = 0
+    completed = 0
+
+    for task in todos.json():
+        if task.get('userId') == int(userId):
+            totalTasks += 1
+            if task.get('completed'):
+                completed += 1
+
+    print('Employee {} is done with tasks({}/{}):'
+          .format(name, completed, totalTasks))
+
+    print('\n'.join(["\t " + task.get('title') for task in todos.json()
+          if task.get('userId') == int(userId) and task.get('completed')]))
